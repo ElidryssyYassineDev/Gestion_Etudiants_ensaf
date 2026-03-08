62ea7f35954e4eced72a0e6b5bac6691c178fb44
ELIDRYSSYyassine.dev <yassineidryssy@gmail.com>
2026-03-08 23:10:58 +0000

    Création du projet ASP.net

---
+++ GestionEtudiants/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GestionEtudiants.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
